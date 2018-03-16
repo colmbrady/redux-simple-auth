b35d3daeef790f7a80ad4a3bce54cb4fda1c5eb9
Jerel Miller <jerelmiller@gmail.com>
2018-03-15 23:28:10 -0700

    Convert createAuthenticator to typescript

---
+++ packages/redux-simple-auth/src/createAuthenticator.ts
@@ -0,0 +1,30 @@
+import invariant from 'invariant'
+import { Authenticator } from './types'
+
+interface options {
+  name: string
+  restore: () => Promise<any>
+  authenticate: () => Promise<any>
+  invalidate: (data: any) => Promise<any>
+}
+
+export default ({
+  name,
+  restore = () => Promise.reject(null),
+  authenticate = () => Promise.reject(null),
+  invalidate = (data: any) => Promise.resolve(data)
+}: options): Authenticator => {
+  invariant(name != null, 'Authenticators must define a `name` property')
+
+  invariant(
+    typeof name === 'string',
+    'Expected the `name` property of the authenticator to be a string'
+  )
+
+  return {
+    name,
+    restore,
+    authenticate,
+    invalidate
+  }
+}
